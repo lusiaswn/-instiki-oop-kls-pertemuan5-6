743fdc41797f881e705acceb4306e8a84cec8a56
lusiaswn <lusiasawun@gmail.com>
2025-04-26 12:05:02 +0800

    Add files via upload

---
+++ pertemuan5&601/pertemuan5&602/src/pertemuan5/pkg602/Pertemuan5602.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan5.pkg602;
+
+/**
+ *
+ * @author INSTIKI
+ */// TGL26-04-2025
+public class Pertemuan5602 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Luassegitiga SegiTiga01 = new Luassegitiga();
+       SegiTiga01.alas = 9;
+       SegiTiga01.tinggi = 7;
+       System.out.print("Luas Segi tiga: "+SegiTiga01.HitungLuas());
+    
+    }
+    
+}
